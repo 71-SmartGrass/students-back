4d6cca6e9a97c04cc65b8442e25c9b9b05f2b5b0
71-SmartGrass <1248397173@qq.com>
2026-05-19 22:12:14 +0800

    Update UserMapper.java

--- src/main/java/com/example/studentsback/mapper/UserMapper.java
+++ src/main/java/com/example/studentsback/mapper/UserMapper.java
@@ -10,4 +10,17 @@ public interface UserMapper {
     // 根据用户名查询用户
     @Select("select * from users where username = #{username}")
     User selectByUsername(@Param("username") String username);
+    
+     // 插入用户
+    @Insert("INSERT INTO users(username, password, role) VALUES(#{username}, #{password}, #{role})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int insert(User user);
+
+    // 根据ID查询用户
+    @Select("SELECT * FROM users WHERE id = #{id}")
+    User selectById(Integer id);
+
+    // 更新用户
+    @Update("UPDATE users SET password = #{password} WHERE id = #{id}")
+    int updatePassword(User user);
 }
