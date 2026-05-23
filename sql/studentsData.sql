3c32c9978b147270bf230493086ea4aa51e13e02
71-SmartGrass <1248397173@qq.com>
2026-05-23 14:53:33 +0800

    Revert "crud"
    This reverts commit 28a28eca2e8d326414bd06307a51daecfd6fee25.

--- sql/studentsData.sql
+++ sql/studentsData.sql
@@ -1,21 +1,3 @@
 use students;
 
--- 用户数据
-insert into users (username, password, role) values
-    ('admin', '123456', 'admin');
-
--- 学生数据
-insert into student (student_number, name, gender, class_name, phone, email) values
-                                                                                 ('202501', '聪明草', '女', '计算机1班', '13800000001', 'smartgrass@test.com'),
-                                                                                 ('202502', '李华', '男', '计算机1班', '13800000002', 'lihua@test.com');
-
--- 课程数据
-insert into course (course_name, course_number, credit, teacher, semester) values
-                                                                               ('高等数学', '108', 5.0, '刘教授', 1),
-                                                                               ('线性代数', '107', 5.0, '卢教授', 1);
-
--- 成绩数据
-insert into score (student_id, course_id, score) values
-                                                     (1, 1, 85),
-                                                     (1, 2, 90),
-                                                     (2, 1, 78);
+insert ignore into users (username, password, role) VALUES ('admin', '123456', 'admin');
